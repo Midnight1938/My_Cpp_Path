39e47b3d1ae08a6c8f97144548a1a3b8b8ec452a
Midnight1938 <dhoomsaksham2004@gmail.com>
2020-12-25 22:09:10 +0530

    For_Loops

---
+++ Basic Syntax/For_Loop/For_lp/For_Lp.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    //for( ; ; ){
+    //    cout << "Infinite" << endl;
+    //}
+
+    for (int i = 0; i < 6; i++) //  for (Before loop; Condition; Execute post loop)
+    {
+        cout << "The 'i' is " << i << " now" << endl;
+    }
+
+    return 0;
+}
