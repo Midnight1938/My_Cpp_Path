4a916bf2d2365a03213fdc61a28bed79c8913e67
Midnight1938 <dhoomsaksham2004@gmail.com>
2020-12-26 20:04:16 +0530

    Improved table

--- Basic Syntax/Arrays/Multip_table.cpp
+++ Basic Syntax/Arrays/Multip_table.cpp
@@ -11,13 +11,13 @@ int main()
 
     for (int i = 1; i <= 10; i++)
     {
-        cout << "\nTable of " << i << endl;
+        cout << "\nTable of " << i << ": " << endl;
         for (int j = 1; j <= 10; j++)
         {
             Multip_tbl[i][0] = i;
             Multip_tbl[i][1] = j;
             Multip_tbl[i][2] = Multip_tbl[i][0] * Multip_tbl[i][1];
-            cout << Multip_tbl[i][0] << " * " << Multip_tbl[i][1] << " = " << Multip_tbl[i][2] << endl;
+            cout << "\t" << Multip_tbl[i][0] << " * " << Multip_tbl[i][1] << " = " << Multip_tbl[i][2] << endl;
         }
     }
 
