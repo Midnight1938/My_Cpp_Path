8f0426220d6563c5fa5dc1695abe1499a53b4ef7
Midnight1938 <dhoomsaksham2004@gmail.com>
2020-12-26 18:54:20 +0530

    Arrays

--- Basic Syntax/Arrays/Arrays.cpp
+++ Basic Syntax/Arrays/Arrays.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 /*
 * Array - List of values in one Variable
+* Index starts from 0
+* Dont overflow strings
 */
 
 int main()
@@ -21,7 +23,8 @@ int main()
     cout << "Array Number 1 is " << ArrayValue[1] << endl;
     cout << "Array Number 3 is " << ArrayValue[3] << endl;
     cout << "Array Number 2 is " << ArrayValue[2] << endl;
-    cout << "\tA non Existent Array 4 is " << ArrayValue[4] << endl;
+    cout << "BAD Idea: " << ArrayValue[4] << endl;
+    cout << "\tA non Existent Array 5 is " << ArrayValue[5] << endl;
 
     float Nummers[4] = {56, 6.7, 78, 8.9}; // Better than the prev approach
 
@@ -29,7 +32,14 @@ int main()
 
     for (int i = 0; i < 5; i++)
     {
-        cout << "Pre-Def at index " << i << ": " << Nummers[i] << endl;
+        if (i == 4)
+        {
+            cout << "\tNon-Existent pre-Def at index " << i << ": " << Nummers[i] << endl;
+        }
+        else
+        {
+            cout << "Pre-Def at index " << i << ": " << Nummers[i] << endl;
+        }
     }
 
     // Some For Magic
@@ -38,9 +48,25 @@ int main()
     for (int i = 0; i < 5; i++)
     {
         double Nummers[4] = {2, 3, 4, 5};
-        cout << "Pre-Def at index " << i << ": " << Nummers[i] << endl;
+        if (i == 4)
+        {
+            cout << "\tNon-Existent array with FOR magic at index " << i << ": " << Nummers[i] << endl;
+        }
+        else
+        {
+            cout << "Array with FOR Magic at index " << i << ": " << Nummers[i] << endl;
+        }
     }
 
+    // A String array
+    string StrArrae[] = {"Apple", "Mango", "Grapes"};
+
+    cout << "\nString Array: \n=====================" << endl;
+
+    for (int i = 0; i < 3; i++) // It will overflow and crash if the i goes beyond
+    {
+        cout << "String is " << StrArrae[i] << endl;
+    }
 
     return 0;
 }
