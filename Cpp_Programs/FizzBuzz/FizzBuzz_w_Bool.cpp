6379b570ab2abd969fee024ff840f26b3208190b
Skshm <eminate894567@gmail.com>
2021-06-04 14:59:35 +0530

    Programs Folded

---
+++ Cpp_Programs/FizzBuzz/FizzBuzz_w_Bool.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+? FIZZBUZZ with Boolean
+*/
+
+int main()
+{
+    for (int i = 0; i <= 100; ++i)
+    {
+        bool Fizz = (i % 3) == 0;
+        bool Buzz = (i % 5) == 0;
+        bool FBuzz = (i % 15) == 0;
+
+        if (FBuzz)
+        {
+            cout<<"FizzBuzz, ";
+        }
+        
+        else if (Fizz)
+        {
+            cout<<"Fizz, ";
+        }
+        else if (Buzz)
+        {
+            cout<<"Buzz, ";
+        }
+        else if(!Fizz && !Buzz && !FBuzz)
+        {
+            cout<<i<<", ";
+        }           
+    }
+     
+
+    return 0;
+}
---
+++ Cpp_Programs/FizzBuzz/fizzBuzz.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+? A Simple FizzBuzz program
+*/
+
+constexpr int Total = 100;
+
+int main()
+{
+    for(int i = 1; i <= Total; i++){
+        if(i%15==0){
+            cout<<"FizzBuzz, ";
+        }
+        else if (i%3==0)
+        {
+            cout<<"Fizz, ";
+        }
+        else if(i%5==0){
+            cout<<"Buzz, ";
+        }
+        else{
+            cout<<i<<", ";
+        }
+        
+    }
+
+    return 0;
+}
