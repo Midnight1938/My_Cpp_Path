485264ea6362f46e07590b2bf38f1c3255bdea43
Skshm <eminate894567@gmail.com>
2021-06-22 13:44:29 +0530

    Charater arrays

--- Section_5:_Pointers_and_Memory/42._Pointer_Arithmetic/Pointer_Arithmetic.cpp
+++ Section_5:_Pointers_and_Memory/42._Pointer_Arithmetic/Pointer_Arithmetic.cpp
@@ -4,17 +4,20 @@ using namespace std;
 
 /*
 ! Pointing off the end of the array may be dangerous
+? '*' signifies a pointer
+
 TODO: Mess around with poiners, add, subt, move around it
 */
 
 int main()
 {
 
-    const int nStrings = 5; //! Const  makes it so that it neverr changes value
-    string texts[nStrings] = {"Ek", "Do", "Teen", "Chaar", "Panch"};
+    const int nStrings = 5;                                           //! Const  makes it so that it neverr changes value
+    string texts[nStrings] = {"Ek", "Do", "Teen", "Chaar", "Panch"}; //* An Array
 
-    string *pTexts = texts;
+    string *pTexts = texts; //? Pointing to the array
 
+    // ? Mesing with pointer position
     cout << *pTexts << endl;
     pTexts = pTexts + 1;
     cout << *pTexts << endl;
@@ -32,19 +35,21 @@ int main()
         cout << *pTexts << endl;
         pTexts++;
     }
-    cout<<"\n"<<endl;
+    cout << "\n"
+         << endl;
 
     //* Setting P back
-    pTexts = &texts[0];
+    pTexts = &texts[0]; //? at address of array[0]
     long Elements = (long)(pEnd - pTexts); // ? Long prevents possible overflows
-    cout << Elements <<"\n"<< endl;
+    cout << Elements << "\n"
+         << endl;
 
     //* Setting P back and reverse
     pTexts = &texts[0];
-    
-    pTexts+= nStrings/2;
-    cout<< pTexts << endl;
-    cout<< *pTexts << endl;
-    
+
+    pTexts += nStrings / 2;
+    cout << pTexts << endl;
+    cout << *pTexts << endl;
+
     return 0;
 }
