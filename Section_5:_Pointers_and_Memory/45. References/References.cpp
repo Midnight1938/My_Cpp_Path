36bfc9c10ae7c3863c7ef18c9facab04e4e725e2
Skshm <eminate894567@gmail.com>
2021-07-04 16:41:19 +0530

    referencing variables and making stuff constant

---
+++ Section_5:_Pointers_and_Memory/45. References/References.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+? The & herre is a different use case compared to pointers, var2 with an &, refers to val1
+? Like an alias; Which means Both point to the same thing
+*/
+
+void ChangedValMachine(double &valuu)
+     //? Without the & in the funciton.
+    //? The 2 variables are seperate.
+{
+    valuu = 123.4;
+}
+
+int main()
+{
+    int val1 = 8;
+    int &val2 = val1; //! int& val2 works too
+    val2 = 10;
+
+    cout << "Value 1:\t" << val1 << endl;
+    cout << "Value 2:\t" << val2 << endl;
+
+    //* USE CASE *//
+
+    double valu = 4.321;
+    ChangedValMachine(valu);
+
+    cout << valu << endl; 
+
+    return 0;
+}
---
+++ Section_5:_Pointers_and_Memory/46. Const/Const.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+* Const just makes the variable unchangable
+* Useful when we want to reduce bug chances
+*/
+
+class Animal //? Just tused a Class rightr here instead of the header file (.h) and going via via
+{
+private:
+    string name;
+
+public: //! Just writen in a line, no big deal
+    void setName(string name) { this->name = name; };
+    void speak() const { cout << "My name is: " << name << endl; } //? Const because instance data doesnt change
+};
+
+int main()
+{
+
+    const double PI = 3.141592;
+    cout << "Const Pi is:\t" << PI << endl;
+
+    //* Class with the const *//
+    Animal animal;
+    animal.setName("Skshm");
+    animal.speak();
+
+    //* Using constant ints *//
+    int value = 6;
+
+      //? const int * const pValue = &value; 
+     //* a constant pointer to a constant int
+    //! Makes it completely constant, so the rest will break
+
+    int *pValue = &value;
+
+    cout << *pValue << endl;
+
+    int number = 11;
+    pValue = &number; //? Error with: int * const pValue = &value;  (constant pointer)
+    *pValue = 12;     //? Error with: const int *pValue = &value;   (constant int)
+
+    cout << *pValue << endl;
+
+    return 0;
+}
