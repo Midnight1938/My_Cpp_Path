0bfaa540d5af6dd8c9e7df89ea63df5381dbfb19
Skshm <eminate894567@gmail.com>
2021-07-20 11:30:34 +0530

    Changing Colours

--- Section_8:_Dev_a_Program/MovingUp/main.cpp
+++ Section_8:_Dev_a_Program/MovingUp/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "SDL2/SDL.h"
+#include "math.h"
 #include "Screen.h"
 
 using namespace std;
@@ -17,6 +18,10 @@ using namespace Screen_Maykr;
 * * Updates now exist in their own sub class, and SetPixel is now used instead of memset duplicate
 ? ? ? Refer to the Shifty Bits for info on SetPixel
 * * * [(y * Scrn_Width) + x] sets the x,y coordinates into an index value in the buffer
+* We use trigonometry (sin) to make the colour dance as it goes from -1 to +1.
+? ? Unsigned char has the max value of 255, which is prefect as hex values cant go above that
+* * Multiplied by 0.0001 to make it a bit uniform. And using natural numbers to make it range btwn 0 and 255
+TODO: Set trigs to sin, cos, tan for funny time
 */
 
 int main()
@@ -29,24 +34,31 @@ int main()
         cout << "Error initialising SDL." << endl;
     }
 
+    int max = 0;
+
     while (true)
     {
         //* Update particles
         //* Draw particles
+
+        //* Magic Colours
+        int Run_Elapse = SDL_GetTicks();
+        unsigned char red = (unsigned char)((1 + sin(Run_Elapse * 0.0002)) * 128);
+        unsigned char blue = (unsigned char)((1 + sin(Run_Elapse * 0.0003)) * 128);
+        unsigned char green = (unsigned char)((1 + sin(Run_Elapse * 0.0001)) * 128);
+
         for (int y = 0; y < Screen::Scrn_Height; y++)
         {
             for (int x = 0; x < Screen::Scrn_Width; x++)
             {
-                PlayArea.setPixel(x, y, 128, 0, 255);
+                PlayArea.setPixel(x, y, red, green, blue);
             }
         }
-
-        PlayArea.setPixel(400, 300, 255, 255, 255);
 
         //* Update Pixels
         PlayArea.Update();
 
-        //* Check for messages/events
+        //* Check for events
         if (PlayArea.processEvents() == false)
         {
             break;
