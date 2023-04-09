1e2158b6c433d3befddefc9e8feafe181545cddb
skshm <eminate894567@protonmail.com>
2023-04-09 18:55:33 +0530

    Whatever

--- build/CMakeFiles/maykr.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for maykr.
-# This may be replaced when dependencies are built.
--- conan_v_vcpkg/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <fmt/format.h>
-#include <iostream>
-
-int main(){
-    std::cout << fmt::format("Which tool is better? {} or {}?\n", "Conan", "Vcpkg");
-    return 0;
-}
